6104c810c50d2d81253d4dca9a0e12d9b45eb096
shuaib88 <shuaibahmed@shuaibs-air.attlocal.net>
2015-03-07 15:49:40 -0600

    sort works, but getting out of range :(

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,21 @@
 import random
 
 def sort(items):
-  # 1. TO DO: Implement a "bubble sort" routine here
-  pass
+  currentNum = items[0]
+  swapCount = 0
+  print(items)
+  print()
 
+  for i in [0,1,2,3,4,5,6,7,8,9]:
+  	currentNum = items[i]
+  	if currentNum > items[i+1]:
+  		items[i], items[i+1] = items[i+1], items[i]
+  		swapCount = swapCount + 1
+  	currentNum = items[i+1]
+  	print("swap count is", swapCount)
+  	print (items)
+  	if swapCount != 0:
+  		sort(items)
 
 numbers = list(range(10))
 random.shuffle(numbers)
@@ -14,3 +26,6 @@ print("The list was sorted correctly!")
 # 2. Change this print statement to display the complexity category.
 #    Refer to the cheat sheet in week9-class for examples.
 print("This algorithm is classified as: O(....)")
+
+
+# another strategy would be do find max and append to new list
---
+++ temp.py
@@ -0,0 +1,5 @@
+blist = [8,1,2]
+
+# blist[0],blist[1] = blist[1],blist[0]
+
+print(blist[3])
